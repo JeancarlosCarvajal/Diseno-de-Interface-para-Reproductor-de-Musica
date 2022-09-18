bc7f91e6eef761b1997354b22c29c358c6ec403e
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-09-18 16:58:37 -0400

    Interface para Reproductor de Musica

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; 
+
+import 'package:j_reproductor_app/src/models/audioplayer_model.dart';
+import 'package:j_reproductor_app/src/pages/music_player_page.dart';
+import 'package:j_reproductor_app/src/theme/theme.dart';
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> new AudioPlayerModel())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Music Player',
+        theme: miTema,
+        home: MusicPlayerPage()
+      ),
+    );
+  }
+}
